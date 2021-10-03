cb33855b3515d1aa83d15b4ec39ba8fcd53f2c29
mohan040102 <78429960+mohan040102@users.noreply.github.com>
2021-10-03 23:03:35 +0530

    Update settings.py

--- cgpa_calculator/settings.py
+++ cgpa_calculator/settings.py
@@ -129,6 +129,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
