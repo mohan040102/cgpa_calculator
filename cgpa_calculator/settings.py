2bdb615097c5527dc3c401d0e241d6fc96601e71
mohan040102 <mohanasubbu2002@gmail.com>
2021-10-03 19:03:03 +0530

    first commit

--- cgpa_calculator/settings.py
+++ cgpa_calculator/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
@@ -123,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
